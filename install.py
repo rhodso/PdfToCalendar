d81c8c8ae895a7146b57f44b684ac87a4ad32f30
rhodso <rhodsoprivate@gmail.com>
2021-08-16 21:47:26 +0100

    Initial commit

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+"""
+    Need to install the following
+    camelot-py[cv]
+    tabula-py
+    Tkinter
+    ghostscript
+"""
+print("Beginning install...")
+os.system("pip3 install camelot-py[cv] tabula-py Tkinter ghostscript")
+print("Done installing")
---
+++ run.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+ECHO "Starting..."
+[PASTE THE OUTPUT OF "where python3" HERE] main.py
+PAUSE
