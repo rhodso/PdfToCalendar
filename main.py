a71daa5c8fb75f16c5cb4df8c1b619592f927e64
rhodso <rhodsoprivate@gmail.com>
2021-10-17 18:52:25 +0100

    New approach

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import tabula
+import json
 import os
 import glob
-import json
 import time
-import csv
+import numpy
 import re
+# import opencv-python
+import pytesseract
+from PIL import Image
+import tabula
 
 #Get a list of all files in current dir
 print("Finding .pdf files")
@@ -24,122 +27,55 @@ print("\nIf all the files you want to search through are not there, press \"Ctrl
 time.sleep(3)
 print("Continuing...")
 time.sleep(0.1)
-filesJson = []
+
+dataWeWant = []
+
+#Start gathering data from files
 for file in files:
-    print("\nProcessing file : \"" + file + "\"")
-    print("Enter page numbers of tables you wish to extract. \nPage numbers can be a single number (ie 8), or can be a range (ie 8-9)\nFor multiple tables, you will need to run this tool again\nAlternativley, type \"skip\" to skip this file")
-    inp = input("")
-    if(inp.lower() == "skip"):
-       continue
-    filesJson.append(tabula.read_pdf(file, pages = inp, multiple_tables=True, output_format="json"))
+    cols = {"Subject": -1, "Date": -1, "Description": -1}
+    print("Starting work on file " + file)
+    pages = input("Enter pages on PDF \""+ file + "\" where the table exists\neg 8, 8-9, all: ")
 
-#Now filesJson conains json data for each table that was extracted
+    #For each heading in the table
+    for key in cols:
+        print("Enter the column number where the " + key + " is\n(Left to Right, with 1 being the 1st column in the table):")
+        while(True):
+            num = input("")
+            if(num.isnumeric()):
+                num = int(num)
+                if(num > 0):
+                    num -= 1
+                    cols[key] = num
+                    break
+            print("Unrecognised input")
+    print("Finished getting columns")
 
-count = 0
-for jsonFile in filesJson:
-    fileName = "tableJson"
-    if(count != 0):
-        fileName += count
+    readJson = tabula.read_pdf(file, pages = pages, output_format="json")[0]
+    newJsonString = str(readJson).replace("'", '"')
+    jsonStrings = []
     
-    with open(fileName + ".json", "w") as outFile:
-        json.dump(jsonFile, outFile)
-
-#Read the JSON we just made in order to convert it to a .csv
-dataWeWant = []
-
-jsonStrings = []
-files = glob.glob("*.json")
-for file in files:
-    f = open(file)
-    jsonStringBS = f.readlines()
-    jsonStringBS = re.sub("[\n\r]","",jsonStringBS[0])
-    data = json.loads(jsonStringBS)
+    data = json.loads(newJsonString)["data"]
     for i in data:
-        jsonStrings.append(i["data"])
+        jsonStrings.append(i)
 
-dataWeWant = []
-for arrays in jsonStrings:
-    for array in arrays:
-        dataFromTable = []
-        for string in array:
-            dataFromTable.append(string["text"])
-        dataWeWant.append(dataFromTable)
+    thisData = []
+    for row in jsonStrings:
+        tmp = []
+        tmp.append(row[cols["Subject"]])
+        tmp.append(row[cols["Date"]])
+        tmp.append(row[cols["Description"]])
+        thisData.append(tmp)
+
+    #Filter data before we add to thisData
+
+    #TODO
+
+print(dataWeWant)
 
 print("Formatting csv")
-
 #CSVStuff
 csvHeaders = ["Subject", "Start Date", "Start Time", "End Date", "End Time", "All day event", "Description", "Location"]
 csvData = []
-
-for row in dataWeWant:
-    csvRowData = []
-    try:
-        if(row[0] == "" or row[0] == "DATE"):
-            continue
-        desc = "Readings: "
-        desc += re.sub("[\n\r]"," ",row[2])
-        desc += " | Assignments: "
-        desc += re.sub("[\n\r]"," ",row[3])
-
-    #Convert row[0] into a date that google calendar likes (DD/MM/YYYY)
-    
-    # Aug 25
-    # 25/08/2021
-        s = re.sub("[\n\r]", " ", row[0]).split(" ")
-        date = s[1]
-    except:
-        continue
-    sub = ""
-    if(s[0] == "Jan"):
-        sub = "1"
-    elif(s[0] == "Feb"):
-        sub = "2"
-    elif(s[0] == "Mar"):
-        sub = "3"
-    elif(s[0] == "Apr"):
-        sub = "4"
-    elif(s[0] == "May"):
-        sub = "5"
-    elif(s[0] == "Jun"):
-        sub = "6"
-    elif(s[0] == "Jul"):
-        sub = "7"
-    elif(s[0] == "Aug"):
-        sub = "8"
-    elif(s[0] == "Sept"):
-        sub = "9"
-    elif(s[0] == "Oct"):
-        sub = "10"
-    elif(s[0] == "Nov"):
-        sub = "11"
-    elif(s[0] == "Dec"):
-        sub = "12"
-    
-    date += "/" + sub + "/2021"
-    
-    #Append data
-    csvRowData.append(re.sub("[\n\r]"," ",row[1]))
-    csvRowData.append(date)
-    csvRowData.append("")
-    csvRowData.append(date)
-    csvRowData.append("")
-    csvRowData.append("TRUE")
-    csvRowData.append(desc)
-    csvRowData.append("")
-
-    #Append to the correct thing
-    csvData.append(csvRowData)
-
-with open("calendarData.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(csvHeaders)
-    writer.writerows(csvData)
-
-#Clean up
-print("Removing excess files...")
-files = glob.glob("*.json")
-for file in files:
-    os.remove(file)
 
 print("Done!")
 print("Take calendarData.csv and upload that to google calendar")
